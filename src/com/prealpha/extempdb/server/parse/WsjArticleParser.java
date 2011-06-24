64282c67b942e89b0f2a16c3992eabc05b4a5d7e
Meyer Kizner <meyer@pre-alpha.com>
2011-06-24 17:19:11 -0400

    WSJ parser uses Jsoup

--- src/com/prealpha/extempdb/server/parse/WsjArticleParser.java
+++ src/com/prealpha/extempdb/server/parse/WsjArticleParser.java
@@ -11,37 +11,33 @@ import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.Namespace;
-import org.jdom.filter.Filter;
-import org.jdom.input.DOMBuilder;
-import org.w3c.tidy.Tidy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.prealpha.extempdb.server.http.HttpClient;
 import com.prealpha.extempdb.server.http.RobotsExclusionException;
 
 final class WsjArticleParser extends AbstractArticleParser {
 	private static enum ArticleType {
-		COMPLETE("articlePage"), ABRIDGED("article story");
+		COMPLETE("div.articlePage > p"), ABRIDGED("div.article > p");
 
-		private final String articleBodyClass;
+		private final String bodySelector;
 
-		private ArticleType(String articleBodyClass) {
-			this.articleBodyClass = articleBodyClass;
+		private ArticleType(String bodySelector) {
+			this.bodySelector = bodySelector;
 		}
 
-		public String getArticleBodyClass() {
-			return articleBodyClass;
+		private String getBodySelector() {
+			return bodySelector;
 		}
 	}
 
@@ -55,15 +51,9 @@ final class WsjArticleParser extends AbstractArticleParser {
 
 	private final HttpClient httpClient;
 
-	private final Tidy tidy;
-
-	private final DOMBuilder builder;
-
 	@Inject
-	public WsjArticleParser(HttpClient httpClient, Tidy tidy, DOMBuilder builder) {
+	private WsjArticleParser(HttpClient httpClient) {
 		this.httpClient = httpClient;
-		this.tidy = tidy;
-		this.builder = builder;
 	}
 
 	@Override
@@ -76,7 +66,8 @@ final class WsjArticleParser extends AbstractArticleParser {
 		try {
 			Map<String, String> params = Collections.emptyMap();
 			InputStream stream = httpClient.doGet(url, params);
-			return getFromHtml(stream);
+			Document document = Jsoup.parse(stream, null, url);
+			return getFromHtml(document);
 		} catch (IOException iox) {
 			throw new ArticleParseException(iox);
 		} catch (RobotsExclusionException rex) {
@@ -84,112 +75,53 @@ final class WsjArticleParser extends AbstractArticleParser {
 		}
 	}
 
-	private ProtoArticle getFromHtml(InputStream html)
+	private static ProtoArticle getFromHtml(Document document)
 			throws ArticleParseException {
-		org.w3c.dom.Document doc = tidy.parseDOM(html, null);
-		doc.removeChild(doc.getDoctype());
-		Document document = builder.build(doc);
-
-		/*
-		 * The first thing we need to do is figure out what article type we're
-		 * dealing with. There are specific meta tags we can look for to figure
-		 * this out.
-		 */
-		ArticleType articleType;
-		Namespace namespace = document.getRootElement().getNamespace();
-		Element headElement = document.getRootElement().getChild("head",
-				namespace);
-		Map<String, String> metaMap = ParseUtils.getMetaMap(headElement);
-
-		String pageType = metaMap.get("displayname");
+		// verify that we have a parseable article
+		String pageType = document.select("meta[name=displayName]").attr(
+				"content");
 		if (UNPARSEABLE_TYPES.contains(pageType)) {
 			return null;
 		}
 
-		if (metaMap.containsKey("GOOGLEBOT")) {
-			articleType = ArticleType.ABRIDGED;
+		// get the article type, needed later
+		ArticleType type;
+		if (document.select("meta[name=GOOGLEBOT]").isEmpty()) {
+			type = ArticleType.COMPLETE;
 		} else {
-			articleType = ArticleType.COMPLETE;
+			type = ArticleType.ABRIDGED;
 		}
 
-		Filter newswireFilter = ParseUtils.getElementFilter("div", "class",
-				"articleHeadlineBox headlineType-newswire");
-		Filter bylineIconFilter = ParseUtils.getElementFilter("div", "class",
-				"articleHeadlineBox headlineType-bylineIcon");
-		Filter headlineBoxFilter = ParseUtils.getOrFilter(newswireFilter,
-				bylineIconFilter);
-		Element headlineBox = (Element) document.getDescendants(
-				headlineBoxFilter).next();
+		String title = document.select("h1").first().text();
 
-		Filter articleBodyFilter = ParseUtils.getElementFilter("div", "class",
-				articleType.getArticleBodyClass());
-		Element articleBody = (Element) document.getDescendants(
-				articleBodyFilter).next();
-
-		String title = getTitle(headlineBox, namespace);
-		String byline = getByline(articleBody, articleType);
-		Date date = getDate(headlineBox);
-		List<String> paragraphs = getParagraphs(articleBody, namespace);
-
-		return new ProtoArticle(title, byline, date, paragraphs);
-	}
-
-	private static String getTitle(Element headlineBox, Namespace namespace) {
-		Element headline = headlineBox.getChild("h1", namespace);
-		return headline.getValue();
-	}
-
-	private static String getByline(Element articleBody, ArticleType articleType) {
-		Filter bylineFilter = ParseUtils.getElementFilter("h3", "class",
-				"byline");
-		Iterator<?> bylineIterator = articleBody.getDescendants(bylineFilter);
-
-		if (!bylineIterator.hasNext()) {
-			return null;
+		Element bylineElem = document.select("h3.byline").first();
+		String byline;
+		if (bylineElem == null) {
+			byline = null;
 		} else {
-			String byline = ((Element) bylineIterator.next()).getValue();
+			byline = bylineElem.text();
 			if (byline.isEmpty()) {
-				return null;
-			} else {
-				return byline;
+				byline = null;
 			}
 		}
-	}
 
-	private static Date getDate(Element headlineBox)
-			throws ArticleParseException {
-		Filter dateStampFilter = ParseUtils.getElementFilter("li", "class",
-				"dateStamp");
-		Filter dateStampFirstFilter = ParseUtils.getElementFilter("li",
-				"class", "dateStamp first");
-		Filter dateElementFilter = ParseUtils.getOrFilter(dateStampFilter,
-				dateStampFirstFilter);
-		Element dateElement = (Element) headlineBox.getDescendants(
-				dateElementFilter).next();
-		String dateStr = dateElement.getValue();
-
+		Date date;
 		try {
-			return DATE_FORMAT.parse(dateStr);
+			String dateStr = document.select("li.dateStamp").first().text();
+			date = DATE_FORMAT.parse(dateStr);
 		} catch (ParseException px) {
 			throw new ArticleParseException(px);
 		}
-	}
 
-	private static List<String> getParagraphs(Element articleBody,
-			Namespace namespace) {
-		// we only want direct children of the article body
-		List<String> paragraphs = new ArrayList<String>();
-		List<?> paragraphElements = articleBody.getChildren("p", namespace);
-
-		for (Object obj : paragraphElements) {
-			Element paragraphElement = (Element) obj;
-			String paragraph = paragraphElement.getValue().trim();
-
-			if (!paragraph.isEmpty()) {
-				paragraphs.add(paragraph);
+		List<String> paragraphs = Lists.newArrayList();
+		List<Element> elements = document.select(type.getBodySelector());
+		for (Element elem : elements) {
+			String text = elem.text().trim();
+			if (!text.isEmpty()) {
+				paragraphs.add(text);
 			}
 		}
 
-		return paragraphs;
+		return new ProtoArticle(title, byline, date, paragraphs);
 	}
 }
