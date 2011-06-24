dac73343477f0393620c15fc2c0dbca93b91c8ed
Meyer Kizner <meyer@pre-alpha.com>
2011-06-24 18:12:45 -0400

    Guardian parser uses Jsoup

--- src/com/prealpha/extempdb/server/parse/GuardianArticleParser.java
+++ src/com/prealpha/extempdb/server/parse/GuardianArticleParser.java
@@ -11,44 +11,48 @@ import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.Namespace;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.prealpha.extempdb.server.http.HttpClient;
 import com.prealpha.extempdb.server.http.RobotsExclusionException;
 
 final class GuardianArticleParser extends AbstractArticleParser {
 	private static enum Feed {
-		NONE(null, DATE_FORMAT_UK, "p") {
+		NONE(null, DATE_FORMAT_UK) {
 		},
 
-		AP("AP foreign", DATE_FORMAT_US, "p") {
+		AP("AP foreign", DATE_FORMAT_US) {
 		},
 
-		REUTERS("Reuters", DATE_FORMAT_US, "div") {
+		REUTERS("Reuters", DATE_FORMAT_US, "div#article-body-blocks > div") {
 		},
 
-		PRESS_ASSOCIATION("Press Association", DATE_FORMAT_US, "p") {
+		PRESS_ASSOCIATION("Press Association", DATE_FORMAT_US) {
 		};
 
 		private final String name;
 
 		private final DateFormat dateFormat;
 
-		private final String textElement;
+		private final String bodySelector;
 
-		private Feed(String name, DateFormat dateFormat, String textElement) {
+		private Feed(String name, DateFormat dateFormat) {
+			this(name, dateFormat, "div#article-body-blocks > p");
+		}
+
+		private Feed(String name, DateFormat dateFormat, String bodySelector) {
 			this.name = name;
 			this.dateFormat = dateFormat;
-			this.textElement = textElement;
+			this.bodySelector = bodySelector;
 		}
 
 		private static Feed fromName(String name) {
@@ -70,7 +74,7 @@ final class GuardianArticleParser extends AbstractArticleParser {
 	private final HttpClient httpClient;
 
 	@Inject
-	public GuardianArticleParser(HttpClient httpClient) {
+	private GuardianArticleParser(HttpClient httpClient) {
 		this.httpClient = httpClient;
 	}
 
@@ -79,80 +83,44 @@ final class GuardianArticleParser extends AbstractArticleParser {
 		try {
 			Map<String, String> params = Collections.emptyMap();
 			InputStream stream = httpClient.doGet(url, params);
-			return getFromHtml(stream);
+			Document document = Jsoup.parse(stream, null, url);
+
+			if (document.body().className().contains("has-badge")) {
+				// blog posts, contests, etc. all seem to contain this
+				return null;
+			}
+
+			String title = document.select("h1").first().text();
+
+			Element bylineElem = document.select("a.contributor").first();
+			String byline = ((bylineElem == null) ? null : bylineElem.text());
+
+			// get both the date and the feed from the publication element
+			Date date;
+			String[] publication = document.select("li.publication").first()
+					.text().split(",");
+			Feed feed = Feed.fromName(publication[0].trim());
+			try {
+				String dateStr = publication[1].trim();
+				date = feed.dateFormat.parse(dateStr);
+			} catch (ParseException px) {
+				throw new ArticleParseException(px);
+			}
+
+			List<String> paragraphs = Lists.newArrayList();
+			List<Element> elements = document.select(feed.bodySelector);
+			for (Element elem : elements) {
+				String text = elem.text().trim();
+				if (!text.isEmpty()) {
+					paragraphs.add(text);
+				}
+			}
+
+			return new ProtoArticle(title, byline, date, paragraphs);
 		} catch (IOException iox) {
 			throw new ArticleParseException(iox);
 		} catch (RobotsExclusionException rex) {
 			throw new ArticleParseException(rex);
 		}
-	}
-
-	private ProtoArticle getFromHtml(InputStream html)
-			throws ArticleParseException {
-		Document document = ParseUtils.parse(html);
-		Namespace namespace = document.getRootElement().getNamespace();
-
-		// if we have a blog post, find out and return null now
-		Element bodyElement = ParseUtils.searchDescendants(document, "body")
-				.get(0);
-		String classAttr = bodyElement.getAttributeValue("class");
-		if (classAttr.contains("has-badge")) {
-			// seems to be a marker for blog posts, contests, etc.
-			return null;
-		}
-
-		// get the title
-		Element titleElement = ParseUtils.searchDescendants(document, "div",
-				"id", "main-article-info").get(0);
-		Element heading = titleElement.getChild("h1", namespace);
-		String title = heading.getValue();
-
-		// get the byline, if there is one
-		List<Element> bylineElements = ParseUtils.searchDescendants(document,
-				"a", "class", "contributor");
-		String byline;
-		if (bylineElements.size() > 0) {
-			Element bylineElement = (Element) bylineElements.get(0);
-			byline = bylineElement.getValue();
-		} else {
-			byline = null;
-		}
-
-		/*
-		 * Get the date. The actual date is in a non-standard <time> element,
-		 * which JTidy doesn't like and seems to remove. So we use the parent
-		 * element and parse out the date. For example, the parent element might
-		 * contain "The Guardian, Thursday 27 January 2011". So we split() on
-		 * the comma and take the second fragment for parsing.
-		 */
-		Element dateElement = ParseUtils.searchDescendants(document, "li",
-				"class", "publication").get(0);
-		String[] dateSplit = dateElement.getValue().split(",");
-		Feed feed = Feed.fromName(dateSplit[0].trim());
-		String dateString = dateSplit[1].trim();
-		Date date;
-		try {
-			date = feed.dateFormat.parse(dateString);
-		} catch (ParseException px) {
-			throw new ArticleParseException(px);
-		}
-
-		// get the body text
-		List<String> paragraphs = new ArrayList<String>();
-		Element articleWrapper = ParseUtils.searchDescendants(document, "div",
-				"id", "article-wrapper").get(0);
-		List<Element> paragraphElements = ParseUtils.searchDescendants(
-				articleWrapper, feed.textElement);
-		for (Element paragraph : paragraphElements) {
-			// Reuters has an additional wrapper div that we have to skip
-			if (paragraph.getChildren(feed.textElement, namespace).isEmpty()) {
-				String text = paragraph.getValue().trim();
-				if (!text.isEmpty()) {
-					paragraphs.add(text);
-				}
-			}
-		}
-
-		return new ProtoArticle(title, byline, date, paragraphs);
 	}
 }
