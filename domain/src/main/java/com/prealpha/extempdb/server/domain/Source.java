4aa5c494087dfa8722958b02d141ce162b71a1d7
Meyer Kizner <meyer@pre-alpha.com>
2011-07-05 03:49:42 -0400

    Add a note

--- domain/src/main/java/com/prealpha/extempdb/server/domain/Source.java
+++ domain/src/main/java/com/prealpha/extempdb/server/domain/Source.java
@@ -8,21 +8,24 @@ package com.prealpha.extempdb.server.domain;
 
 import static com.google.common.base.Preconditions.*;
 
+/*
+ * TODO: using an enum means that adding new sources breaks interoperability
+ */
 public enum Source {
 	NY_TIMES("www.nytimes.com", "New York Times"),
-	
+
 	WASHINGTON_POST("www.washingtonpost.com", "Washington Post"),
-	
+
 	CS_MONITOR("www.csmonitor.com", "Christian Science Monitor"),
-	
+
 	WS_JOURNAL("online.wsj.com", "Wall Street Journal"),
-	
+
 	REUTERS("www.reuters.com", "Reuters"),
-	
+
 	GUARDIAN("www.guardian.co.uk", "The Guardian"),
-	
+
 	ECONOMIST("www.economist.com", "The Economist");
-	
+
 	public static Source fromDomainName(String domainName) {
 		checkNotNull(domainName);
 		for (Source source : values()) {
@@ -32,20 +35,20 @@ public enum Source {
 		}
 		return null;
 	}
-	
+
 	private final String domainName;
-	
+
 	private final String displayName;
-	
+
 	private Source(String domainName, String displayName) {
 		this.domainName = domainName;
 		this.displayName = displayName;
 	}
-	
+
 	public String getDomainName() {
 		return domainName;
 	}
-	
+
 	@Override
 	public String toString() {
 		return displayName;
