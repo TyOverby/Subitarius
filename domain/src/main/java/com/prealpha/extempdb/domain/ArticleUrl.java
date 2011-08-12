dfd01f962a3c821706ea9f7cc13bee691a1a4000
Meyer Kizner <meyer@pre-alpha.com>
2011-08-11 20:17:59 -0700

    Looks like I have to map Article, otherwise Hibernate complains now

--- domain/src/main/java/com/prealpha/extempdb/domain/ArticleUrl.java
+++ domain/src/main/java/com/prealpha/extempdb/domain/ArticleUrl.java
@@ -57,6 +57,7 @@ public class ArticleUrl extends DistributedEntity {
 		String domainName = rawUrl.substring(7, index);
 		Source source = Source.fromDomainName(domainName);
 		url = source.canonicalize(rawUrl);
+		articles = ImmutableSet.of();
 		mappings = ImmutableSet.of();
 	}
 
@@ -89,8 +90,12 @@ public class ArticleUrl extends DistributedEntity {
 						return (input.getChild() == null);
 					}
 				});
-		checkState(headArticles.size() == 1);
-		return headArticles.iterator().next();
+		if (headArticles.isEmpty()) {
+			return null;
+		} else {
+			checkState(headArticles.size() == 1);
+			return headArticles.iterator().next();
+		}
 	}
 
 	@OneToMany(mappedBy = "articleUrl")
