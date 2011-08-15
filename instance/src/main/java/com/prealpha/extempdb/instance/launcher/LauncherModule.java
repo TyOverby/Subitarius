f756756497aa00b8fb30b93dfef3bcfbc7172906
Meyer Kizner <meyer@pre-alpha.com>
2011-08-14 23:58:46 -0700

    Going to need this module to get anywhere at runtime

---
+++ instance/src/main/java/com/prealpha/extempdb/instance/launcher/LauncherModule.java
@@ -0,0 +1,35 @@
+/*
+ * LauncherModule.java
+ * Copyright (C) 2011 Meyer Kizner
+ * All rights reserved.
+ */
+
+package com.prealpha.extempdb.instance.launcher;
+
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+import com.prealpha.extempdb.domain.DistributedEntity;
+
+public final class LauncherModule extends AbstractModule {
+	@Override
+	protected void configure() {
+		bind(UserAction.class).toProvider(UserActionContext.class).in(
+				Singleton.class);
+		bind(new TypeLiteral<Iterator<DistributedEntity>>() {
+		}).to(EntityIterator.class);
+		bind(new TypeLiteral<Iterator<Action>>() {
+		}).to(ActionIterator.class);
+	}
+
+	@Provides
+	ExecutorService getThreadPool() {
+		int threads = Runtime.getRuntime().availableProcessors();
+		return Executors.newFixedThreadPool(threads);
+	}
+}
