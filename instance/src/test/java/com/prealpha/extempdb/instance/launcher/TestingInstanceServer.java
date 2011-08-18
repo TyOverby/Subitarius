3771e53d7c7e10098a00e7147fe3eafbbc1d0f3a
Meyer Kizner <meyer@pre-alpha.com>
2011-08-18 01:15:56 -0700

    Make it actually work

--- instance/src/test/java/com/prealpha/extempdb/instance/launcher/TestingInstanceServer.java
+++ instance/src/test/java/com/prealpha/extempdb/instance/launcher/TestingInstanceServer.java
@@ -8,6 +8,8 @@ package com.prealpha.extempdb.instance.launcher;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.google.gwt.dev.DevMode;
 import com.google.inject.Inject;
@@ -17,21 +19,39 @@ import com.prealpha.extempdb.instance.launcher.TestingLauncherModule.ServerUrl;
 final class TestingInstanceServer implements InstanceServer {
 	private final Provider<DevMode> devModeProvider;
 
+	private final Timer timer;
+
 	private final URL serverUrl;
 
 	private DevMode devMode;
 
 	@Inject
 	private TestingInstanceServer(Provider<DevMode> devModeProvider,
-			@ServerUrl String serverUrl) throws MalformedURLException {
+			Timer timer, @ServerUrl String serverUrl)
+			throws MalformedURLException {
 		this.devModeProvider = devModeProvider;
+		this.timer = timer;
 		this.serverUrl = new URL(serverUrl);
 	}
 
 	@Override
-	public synchronized URL start() {
-		devMode = devModeProvider.get();
-		devMode.run();
+	public URL start() {
+		synchronized (this) {
+			devMode = devModeProvider.get();
+		}
+
+		// start() is going to be called on the Swing thread
+		// however, DevMode uses EventQueue.invokeAndWait internally
+		// so we need to run() on some other thread for it to not complain
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				synchronized (TestingInstanceServer.this) {
+					devMode.run();
+				}
+			}
+		}, 0);
+
 		return serverUrl;
 	}
 
