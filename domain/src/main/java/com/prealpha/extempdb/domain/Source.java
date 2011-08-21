97d108925ef10a860a2fd73035957c17142027ce
Meyer Kizner <meyer@pre-alpha.com>
2011-08-21 18:31:33 -0400

    Update change log and a little cleanup

--- domain/src/main/java/com/prealpha/extempdb/domain/Source.java
+++ domain/src/main/java/com/prealpha/extempdb/domain/Source.java
@@ -6,7 +6,7 @@
 
 package com.prealpha.extempdb.domain;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.*;
 
 /*
  * TODO: using an enum means that adding new sources breaks interoperability
@@ -62,7 +62,7 @@ public enum Source {
 	ECONOMIST("www.economist.com", "The Economist") {
 	},
 
-	ALJAZEERA("english.aljazeera.net", "Al Jazeera") {
+	AL_JAZEERA("english.aljazeera.net", "Al Jazeera") {
 	},
 
 	BBC("www.bbc.co.uk", "BBC") {
