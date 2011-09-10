b476799bd3e5fb3591722f71ba8611db8bc672ea
Meyer Kizner <meyer@pre-alpha.com>
2011-09-09 20:37:37 -0400

    This class really doesn't have to be very complicated

--- instance/src/main/java/com/subitarius/instance/server/GuiceServiceServlet.java
+++ instance/src/main/java/com/subitarius/instance/server/GuiceServiceServlet.java
@@ -1,90 +1,53 @@
 /*
  * GuiceServiceServlet.java
- * Copyright (C) 2010 Meyer Kizner
+ * Copyright (C) 2011 Meyer Kizner
  * All rights reserved.
  */
 
 package com.subitarius.instance.server;
 
-import static com.google.common.base.Preconditions.*;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import org.slf4j.Logger;
 
 import com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException;
+import com.google.gwt.user.client.rpc.RpcTokenException;
 import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.server.rpc.RPC;
 import com.google.gwt.user.server.rpc.RPCRequest;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.subitarius.util.logging.InjectLogger;
 
-class GuiceServiceServlet extends RemoteServiceServlet {
+final class GuiceServiceServlet extends RemoteServiceServlet {
 	private static final long serialVersionUID = 1L;
 
-	@Inject
-	private Injector injector;
+	@InjectLogger
+	private Logger log;
 
-	public GuiceServiceServlet() {
+	private final Injector injector;
+
+	@Inject
+	private GuiceServiceServlet(Injector injector) {
+		this.injector = injector;
 	}
 
 	@Override
-	public final String processCall(String payload)
-			throws SerializationException {
-		RPCRequest rpcRequest = null;
-		Method method = null;
-
+	public String processCall(String payload) throws SerializationException {
+		// see the superclass implementation
+		// we basically just substitute our own delegate for the default one
 		try {
-			rpcRequest = RPC.decodeRequest(payload, null, this);
-
-			checkNotNull(rpcRequest, "No RPC request");
-
-			method = rpcRequest.getMethod();
-
-			preInvoke(rpcRequest);
-			Object returnValue = invoke(rpcRequest);
-			returnValue = sanitizeValue(returnValue);
-
-			return RPC.encodeResponseForSuccess(method, returnValue,
-					rpcRequest.getSerializationPolicy());
-		} catch (IllegalArgumentException iax) {
-			String message = "Blocked attempt to invoke method " + method;
-			SecurityException securityException = new SecurityException(message);
-			securityException.initCause(iax);
-			throw securityException;
-		} catch (IllegalAccessException iax) {
-			String message = "Blocked attempt to access inaccessible method "
-					+ method;
-			SecurityException securityException = new SecurityException(message);
-			securityException.initCause(iax);
-			throw securityException;
-		} catch (InvocationTargetException itx) {
-			Throwable thrown = sanitizeValue(itx.getCause());
-			return RPC.encodeResponseForFailure(method, thrown,
-					rpcRequest.getSerializationPolicy());
+			RPCRequest request = RPC.decodeRequest(payload, null, this);
+			Object delegate = injector.getInstance(request.getMethod()
+					.getDeclaringClass());
+			return RPC.invokeAndEncodeResponse(delegate, request.getMethod(),
+					request.getParameters(), request.getSerializationPolicy(),
+					request.getFlags());
 		} catch (IncompatibleRemoteServiceException irsx) {
-			Exception exception = sanitizeValue(irsx);
-			return RPC.encodeResponseForFailure(null, exception,
-					rpcRequest.getSerializationPolicy());
+			log.warn("incompatible remote service", irsx);
+			return RPC.encodeResponseForFailure(null, irsx);
+		} catch (RpcTokenException rtx) {
+			log.warn("invalid RPC token", rtx);
+			return RPC.encodeResponseForFailure(null, rtx);
 		}
-	}
-
-	protected void preInvoke(RPCRequest request) {
-	}
-
-	protected Object invoke(RPCRequest rpcRequest)
-			throws IllegalAccessException, InvocationTargetException {
-		Method method = rpcRequest.getMethod();
-
-		Class<?> serviceClass = method.getDeclaringClass();
-		Object service = injector.getInstance(serviceClass);
-
-		Object[] parameters = rpcRequest.getParameters();
-
-		return method.invoke(service, parameters);
-	}
-
-	protected <T> T sanitizeValue(T value) {
-		return value;
 	}
 }
