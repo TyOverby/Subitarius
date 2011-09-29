2f461d7f43cc72ecd03685ece80131c30511c692
Meyer Kizner <meyer@pre-alpha.com>
2011-09-29 18:00:53 -0400

    Modify the unit test to include mappings, which is the typical use case

--- central/src/test/java/com/subitarius/central/DistributedEntityServletTest.java
+++ central/src/test/java/com/subitarius/central/DistributedEntityServletTest.java
@@ -32,6 +32,7 @@ import org.junit.runner.RunWith;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.inject.Module;
@@ -45,11 +46,16 @@ import com.mycila.testing.plugin.guice.ModuleProvider;
 import com.subitarius.domain.ArticleUrl;
 import com.subitarius.domain.DistributedEntity;
 import com.subitarius.domain.DomainModule;
+import com.subitarius.domain.Tag;
+import com.subitarius.domain.Tag.Type;
+import com.subitarius.domain.TagMapping;
 import com.subitarius.util.logging.TestLoggingModule;
 
 @RunWith(MycilaJunitRunner.class)
 @GuiceContext({ TestLoggingModule.class })
 public final class DistributedEntityServletTest {
+	private static final int URL_COUNT = 50;
+
 	@SuppressWarnings("unused")
 	@ModuleProvider
 	private static Iterable<Module> getModules() {
@@ -72,20 +78,29 @@ public final class DistributedEntityServletTest {
 	@Mock(Mock.Type.STANDARD)
 	private HttpServletResponse res;
 
-	private List<ArticleUrl> urls;
+	private List<DistributedEntity> entities;
 
 	@Before
 	public void setUp() {
 		persistService.start();
 		EntityManager entityManager = entityManagerProvider.get();
 		entityManager.getTransaction().begin();
-		urls = Lists.newArrayListWithCapacity(50);
-		for (int i = 0; i < 50; i++) {
+
+		Tag tag = new Tag("Fake URLs", Type.SEARCHED, ImmutableSet.<Tag> of());
+		entityManager.persist(tag);
+
+		entities = Lists.newArrayListWithCapacity(2 * URL_COUNT);
+		for (int i = 0; i < URL_COUNT; i++) {
 			ArticleUrl url = new ArticleUrl("http://www.nytimes.com/article"
 					+ i);
 			entityManager.persist(url);
-			urls.add(url);
+			entities.add(url);
+
+			TagMapping mapping = new TagMapping(tag, url);
+			entityManager.persist(mapping);
+			entities.add(mapping);
 		}
+
 		entityManager.getTransaction().commit();
 	}
 
@@ -100,7 +115,7 @@ public final class DistributedEntityServletTest {
 		expect(req.getParameter("version")).andReturn("0.2-alpha");
 		expect(req.getParameter("timestamp")).andReturn(null);
 		expect(req.getParameter("prefix")).andReturn(null);
-		testDoGetEntities(urls);
+		testDoGetEntities(entities);
 	}
 
 	@Test
@@ -116,15 +131,15 @@ public final class DistributedEntityServletTest {
 	@Test
 	public void testDoGetPrefix() throws IOException, ServletException,
 			ClassNotFoundException {
-		final String prefix = urls.get(0).getHash().substring(0, 1);
+		final String prefix = entities.get(0).getHash().substring(0, 1);
 		expect(req.getParameter("version")).andReturn("0.2-alpha");
 		expect(req.getParameter("timestamp")).andReturn(null);
 		expect(req.getParameter("prefix")).andReturn(prefix);
-		testDoGetEntities(Collections2.filter(urls,
-				new Predicate<ArticleUrl>() {
+		testDoGetEntities(Collections2.filter(entities,
+				new Predicate<DistributedEntity>() {
 					@Override
-					public boolean apply(ArticleUrl url) {
-						return (url.getHash().startsWith(prefix));
+					public boolean apply(DistributedEntity entity) {
+						return (entity.getHash().startsWith(prefix));
 					}
 				}));
 	}
@@ -132,19 +147,20 @@ public final class DistributedEntityServletTest {
 	@Test
 	public void testDoGetBoth() throws IOException, ServletException,
 			ClassNotFoundException {
-		final long timestamp = urls.get(25).getPersistDate().getTime();
-		final String prefix = urls.get(25).getHash().substring(0, 1);
+		final long timestamp = entities.get(URL_COUNT).getPersistDate()
+				.getTime();
+		final String prefix = entities.get(URL_COUNT).getHash().substring(0, 1);
 		expect(req.getParameter("version")).andReturn("0.2-alpha");
 		expect(req.getParameter("timestamp")).andReturn(
 				Long.toString(timestamp));
 		expect(req.getParameter("prefix")).andReturn(prefix);
-		testDoGetEntities(Collections2.filter(urls,
-				new Predicate<ArticleUrl>() {
+		testDoGetEntities(Collections2.filter(entities,
+				new Predicate<DistributedEntity>() {
 					@Override
-					public boolean apply(ArticleUrl url) {
-						boolean timestampApplies = (url.getPersistDate()
+					public boolean apply(DistributedEntity entity) {
+						boolean timestampApplies = (entity.getPersistDate()
 								.getTime() >= timestamp);
-						boolean prefixApplies = (url.getHash()
+						boolean prefixApplies = (entity.getHash()
 								.startsWith(prefix));
 						return (timestampApplies && prefixApplies);
 					}
