4e4b6f80ba29b92b6b55a07060190b047acea5f8
Meyer Kizner <meyer@pre-alpha.com>
2011-10-07 19:37:58 -0400

    Add a PageType enum to handle the new page type

--- instance/src/main/java/com/subitarius/instance/server/parse/NyTimesArticleParser.java
+++ instance/src/main/java/com/subitarius/instance/server/parse/NyTimesArticleParser.java
@@ -19,7 +19,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
@@ -31,10 +30,66 @@ import com.subitarius.util.http.RobotsExclusionException;
 import com.subitarius.util.http.SimpleHttpClient;
 
 final class NyTimesArticleParser implements ArticleParser {
+	private static enum PageType {
+		ARTICLE {
+			@Override
+			String getTitle(Document document) {
+				return document.select("meta[name=hdl]").attr("content");
+			}
+
+			@Override
+			String getByline(Document document) {
+				return document.select("meta[name=byl]").attr("content");
+			}
+
+			@Override
+			Date getDate(Document document) throws ParseException {
+				String dateStr = document.select("meta[name=pdate]").attr(
+						"content");
+				return DATE_FORMAT.parse(dateStr);
+			}
+
+			@Override
+			List<Element> getElements(Document document) {
+				return document.select("div.articleBody p");
+			}
+		},
+
+		SCHOOLBOOK {
+			@Override
+			String getTitle(Document document) {
+				return document.select("h1.sbook-headline").first().text();
+			}
+
+			@Override
+			String getByline(Document document) {
+				return document.select("p.sbook-bylines").first().text();
+			}
+
+			@Override
+			Date getDate(Document document) throws ParseException {
+				String dateStr = document.select("p.sbook-bydate").first()
+						.text();
+				return DATE_FORMAT.parse(dateStr);
+			}
+
+			@Override
+			List<Element> getElements(Document document) {
+				return document.select("div.sbook-post-content");
+			}
+		};
+
+		abstract String getTitle(Document document);
+
+		abstract String getByline(Document document);
+
+		abstract Date getDate(Document document) throws ParseException;
+
+		abstract List<Element> getElements(Document document);
+	}
+
 	private static final DateFormat DATE_FORMAT = new SimpleDateFormat(
 			"yyyyMMdd");
-
-	private static final List<String> UNPARSEABLE_TYPES = ImmutableList.of();
 
 	private final Provider<Team> teamProvider;
 
@@ -52,25 +107,33 @@ final class NyTimesArticleParser implements ArticleParser {
 		try {
 			Document document = getDocument(articleUrl.getUrl());
 
-			String pageType = document.select("meta[name=PST]").attr("content");
-			if (UNPARSEABLE_TYPES.contains(pageType)) {
-				return null;
+			PageType pageType = null;
+			String pageTypeStr = document.select("meta[name=PT]").attr(
+					"content");
+			if (pageTypeStr.equals("Article")) {
+				pageType = PageType.ARTICLE;
+			} else if (pageTypeStr.equals("Blogs")) {
+				String blogName = document.select("meta[name=BN]").attr(
+						"content");
+				if (blogName.equals("schoolbook")) {
+					pageType = PageType.SCHOOLBOOK;
+				}
+			}
+			if (pageType == null) {
+				throw new ArticleParseException(articleUrl);
 			}
 
-			String title = document.select("meta[name=hdl]").attr("content");
-			String byline = document.select("meta[name=byl]").attr("content");
+			String title = pageType.getTitle(document);
+			String byline = pageType.getByline(document);
 			Date date;
-
 			try {
-				String dateStr = document.select("meta[name=pdate]").attr(
-						"content");
-				date = DATE_FORMAT.parse(dateStr);
+				date = pageType.getDate(document);
 			} catch (ParseException px) {
 				throw new ArticleParseException(articleUrl, px);
 			}
 
 			List<String> paragraphs = Lists.newArrayList();
-			List<Element> elements = document.select("div.articleBody p");
+			List<Element> elements = pageType.getElements(document);
 			for (Element elem : elements) {
 				String text = elem.text().trim();
 				// NY Times uses U+0095 as a dot, which MySQL doesn't like
